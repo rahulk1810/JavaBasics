75e2ad082b2e0a21aa7497b9cfba9f01854abde1
Rahul Kashyap <rahulkcloud18@gmail.com>
2023-01-27 20:20:19 +0530

    tested-code

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello"); 
         System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
